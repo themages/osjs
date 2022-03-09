d2f974601bfb5114fea155fd76b8dc3d1ffb9a43
zengxingqi <me@zengxingqi.com>
2022-03-09 20:25:14 +0800

    feat: add cos api

--- serve/cos/sts-server.js
+++ serve/cos/sts-server.js
@@ -1,31 +1,7 @@
-var bodyParser = require('body-parser')
 var STS = require('./sts.js')
+var config = require('../config/cos')
 var express = require('express')
-
-// 配置参数
-var config = {
-	secretId: '',
-	secretKey: '',
-	proxy: '',
-	durationSeconds: 1800,
-
-	// 放行判断相关参数
-	bucket: '',
-	region: '',
-	allowPrefix: 'like', // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径，例子： a.jpg 或者 a/* 或者 * (使用通配符*存在重大安全风险, 请谨慎评估使用)
-	// 简单上传和分片，需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
-	allowActions: [
-		// 简单上传
-		'name/cos:PutObject',
-		'name/cos:PostObject',
-		// 分片上传
-		'name/cos:InitiateMultipartUpload',
-		'name/cos:ListMultipartUploads',
-		'name/cos:ListParts',
-		'name/cos:UploadPart',
-		'name/cos:CompleteMultipartUpload',
-	],
-}
+var bodyParser = require('body-parser')
 
 // 创建临时密钥服务
 var app = express()
