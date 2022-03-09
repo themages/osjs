00bf48683ec1dddaba2ea07e611504b84099d394
Eric <eric_zxq@meten.com>
2022-03-09 19:08:05 +0800

    feat: add cos api

---
+++ client/cos/appendObject.js
@@ -0,0 +1,15 @@
+import { init } from './init'
+// 追加上传对象
+export function appendObject(obj = {}) {
+    const { Bucket, Region, Key, Body, Position, response } = obj
+    init().appendObject(
+        {
+            Bucket /* 填入您自己的存储桶，必须字段 */,
+            Region /* 存储桶所在地域，例如ap-beijing，必须字段 */,
+            Key /* 存储在桶里的对象键（例如1.jpg，a/b/test.txt），必须字段 */,
+            Body, // 上传文件对象
+            Position // 初次上传为0
+        },
+        response // err, data
+    )
+}
---
+++ client/cos/init.js
@@ -0,0 +1,43 @@
+const COS = require('cos-js-sdk-v5')
+let cos = void 0
+// url 替换成您自己的后端服务
+export function init(obj) {
+    const { url = 'http://127.0.0.1:3002/sts' } = obj
+    if (!cos) {
+        cos = new COS({
+            // getAuthorization 必选参数
+            getAuthorization: function (options, callback) {
+                // 异步获取临时密钥
+                // 服务端 JS 和 PHP 例子：https://github.com/tencentyun/cos-js-sdk-v5/blob/master/server/
+                // 服务端其他语言参考 COS STS SDK ：https://github.com/tencentyun/qcloud-cos-sts-sdk
+                // STS 详细文档指引看：https://cloud.tencent.com/document/product/436/14048
+                const xhr = new XMLHttpRequest()
+                let data = void 0,
+                    credentials = void 0
+                xhr.open('GET', url, true)
+                xhr.onload = function (e) {
+                    try {
+                        data = JSON.parse(e.target.responseText)
+                        credentials = data.credentials
+                    } catch (e) {}
+                    if (!data || !credentials) {
+                        return console.error(
+                            'credentials invalid:\n' + JSON.stringify(data, null, 2)
+                        )
+                    }
+                    callback({
+                        TmpSecretId: credentials.tmpSecretId,
+                        TmpSecretKey: credentials.tmpSecretKey,
+                        SecurityToken: credentials.sessionToken,
+                        // 建议返回服务器时间作为签名的开始时间，避免用户浏览器本地时间偏差过大导致签名错误
+                        StartTime: data.startTime, // 时间戳，单位秒，如：1580000000
+                        ExpiredTime: data.expiredTime // 时间戳，单位秒，如：1580000000
+                    })
+                }
+                xhr.send()
+            }
+        })
+        console.log('cos: %O', cos)
+    }
+    return cos
+}
