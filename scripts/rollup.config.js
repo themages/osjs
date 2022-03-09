fb7af324b8a523d99bc515155f9bf0f080b7553d
zengxingqi <me@zengxingqi.com>
2022-03-09 22:22:33 +0800

    feat: update rollup

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,13 +1,13 @@
 // https://www.rollupjs.com/guide/big-list-of-options
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-const version = require("../package.json").version;
-const input = "src/index.js";
-const plugins = [nodeResolve(), commonjs(), terser()];
-const external = [];
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+const version = require('../package.json').version
+const input = 'src/index.js'
+const plugins = [nodeResolve(), commonjs(), terser()]
+const external = []
 const watch = {
-  exclude: "node_modules/**",
-};
+	exclude: 'node_modules/**',
+}
 
-export { input, plugins, external, watch, version };
+export { input, plugins, external, watch, version }
