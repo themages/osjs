88e6ecb4f1e2cdf1bd757089b399eabd443ab229
zengxingqi <me@zengxingqi.com>
2022-03-09 22:50:22 +0800

    feat: use case

--- src/index.js
+++ src/index.js
@@ -1,24 +1,3 @@
-// import { init } from '../client/cos/init.js'
-import { PutObject } from '../client/cos/putObject.js'
+import { init } from '../client/cos/init.js'
 
-var files = document.querySelector('#files')
-files.addEventListener('change', function (e) {
-	console.log(e.target.files)
-	const files = e.target.files[0]
-	PutObject({
-		Bucket: 'sls-cloudfunction-ap-guangzhou-code-1256070452',
-		Region: 'ap-guangzhou',
-		Key: 'like/' + files.name,
-		StorageClass: 'STANDARD',
-		Body: files,
-		onProgress: function (progress) {
-			console.log('progress: %O', progress)
-		},
-		onTaskReady: function (task) {
-			console.log('task: %O', task)
-		},
-		response: function (error, response) {
-			console.log('error: %O, response: %O', error, response)
-		},
-	})
-})
+export const cos_init = init
